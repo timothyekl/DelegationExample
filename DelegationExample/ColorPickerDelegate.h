5818a0e43c0eb6475ea157fd2e1cb5dda03ac0aa
Tim Ekl <lithium3141@gmail.com>
2011-01-27 23:19:48 -0500

    Apply copyright

--- DelegationExample/ColorPickerDelegate.h
+++ DelegationExample/ColorPickerDelegate.h
@@ -3,7 +3,7 @@
 //  DelegationExample
 //
 //  Created by Tim Ekl on 1/27/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Tim Ekl. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
